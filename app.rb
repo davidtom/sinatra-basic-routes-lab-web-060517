5405eb1d72f0d5fa74de3fcb6a437353d13f5a15
davidtom <davidjtomczyk@gmail.com>
2017-06-26 15:01:51 -0400

    complete lab

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is David"
+  end
+
+  get "/hometown" do
+    "My hometown is Stevensville, MI"
+  end
+
+  get "/favorite-song"do
+    "My favorite song is Mr. Jones by the Counting Crows"
+  end
 end
